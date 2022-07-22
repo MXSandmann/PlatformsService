0da46120d80c4712999715590f39b31166cb40a5
mxchainsaw <quit>
2022-07-22 23:29:54 +0200

    Add data layer

---
+++ Data/PlatformRepository.cs
@@ -0,0 +1,37 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepository : IPlatformRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Platform platform)
+        {
+            if (platform == null)
+                throw new ArgumentNullException(nameof(platform));
+
+            _context.Platforms.Add(platform);
+        }
+
+        public Platform Get(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public IEnumerable<Platform> GetAll()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);  
+        }
+    }
+}
